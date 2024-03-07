ec3f05ffcc45d7b783b9175ea76c7fee9602ff39
lfaitanin <lfaitanin@outlook.com>
2024-03-07 00:18:56 -0300

    netlify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as serverless from 'serverless-http';
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT, '0.0.0.0');
-}
-bootstrap();
+const bootstrap = async (module: any) => {
+  const app = express();
+  const nestApp = await NestFactory.create(module, new ExpressAdapter(app));
+
+  nestApp.setGlobalPrefix('/.netlify/functions/server');
+  nestApp.enableCors();
+  nestApp.use(helmet());
+  nestApp.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  nestApp.use(express.json({ limit: '50mb' }));
+  nestApp.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+  await nestApp.init();
+  return app;
+};
+
+let cachedHadler: any;
+const proxyApi = async (module: any, event: any, context: any) => {
+  if (!cachedHadler) {
+    const app = await bootstrap(module);
+    cachedHadler = serverless(app);
+  }
+
+  return cachedHadler(event, context);
+};
+
+export const handler = async (event: any, context: any) =>
+  proxyApi(AppModule, event, context);
