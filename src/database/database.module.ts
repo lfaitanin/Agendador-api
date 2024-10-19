30ced68a1ad3940991551a30273ce231b8ca7172
lfaitanin <lfaitanin@outlook.com>
2024-10-19 09:14:57 -0300

    commit teste

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -9,12 +9,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get('POSTGRES_HOST'),
-        port: configService.get('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USER'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DB'),
-        entities: [__dirname + '/../**/*.entity.{js,ts}'],
+        url: 'postgresql://agendador:LEw3b4nmPGCIqdNW5VNP6JQWJLOMiaaQ@dpg-cs9igbrqf0us739gorn0-a.oregon-postgres.render.com/agendador',
         synchronize: true,
         ssl: true,
       }),
